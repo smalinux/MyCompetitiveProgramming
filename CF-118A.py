e57f49fae47eeab35a453707761c7502a52e4b97
Sohaib Mohamed <sohaib.amhmd@gmail.com>
2021-06-04 04:27:35 +0200

    CF-118A

---
+++ CF-118A.py
@@ -0,0 +1,11 @@
+# https://codeforces.com/contest/118/submission/118320760
+s = input()
+s = s.lower()
+s2 = []
+for i in s:
+    if i not in ['a', 'o', 'y', 'e', 'u', 'i']:
+        s2.append(i)
+
+for i in s2:
+    print(f".{i}", end='')
+
